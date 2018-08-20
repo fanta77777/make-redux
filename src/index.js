2d532d2697dee77c8ef88a100848667558274092
fanta77777 <472849306@qq.com>
2018-08-20 18:01:33 +0800

    make-redux xiaoshu

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+function createStore (state, stateChanger) {
+  const getState = () => state
+  const dispatch = (action) => stateChanger(state, action)
+  return { getState, dispatch }
+}
+
+function stateChanger (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+const store = createStore(appState, stateChanger)
